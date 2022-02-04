b86be5faafbb5c1e71e4c5946606b8d27b4086a8
Metris <metris@Metriss-Mac-mini.local>
2022-02-04 15:00:19 -0300

    first commit

---
+++ InternshipProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InternshipProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
