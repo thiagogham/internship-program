6b051f1f4c99972d979902a70bd91be8da24119d
Thiago Rodrigues Gham <thiago.gham@gmail.com>
2022-02-09 22:44:44 -0300

    Update VeryEasyProgram.cs

--- InternshipProgram/VeryEasy/VeryEasyProgram.cs
+++ InternshipProgram/VeryEasy/VeryEasyProgram.cs
@@ -4,46 +4,73 @@ namespace InternshipProgram.VeryEasy
 {
     public static class VeryEasyProgram
     {
+        /// <summary>
+        /// Method that returns true
+        /// </summary>
         public static bool ReturnTrue()
         {
             throw new NotImplementedException();
         }
-
+        
+        /// <summary>
+        /// Create a function that takes two numbers as arguments and return their sum
+        /// </summary>
         public static int Sum(int a, int b)
         {
             throw new NotImplementedException();
         }
-
+        
+        /// <summary>
+        /// Write a function that takes an integer minutes and converts it to seconds
+        /// </summary>
         public static int ConverMinutesToSeconds(int minutes)
         {
             throw new NotImplementedException();
         }
-
+        
+        /// <summary>
+        /// Create a function that returns true if an integer is evenly divisible by 5, and false otherwise
+        /// </summary>
         public static bool DivisibleByFive(int number)
         {
             throw new NotImplementedException();
         }
-
+        
+        /// <summary>
+        /// Create a function that takes two numbers and a mathematical operator and returns the result
+        /// </summary>
         public static int Calculate(int num1, int num2, string operation)
         {
             throw new NotImplementedException();
         }
-
+        
+        /// <summary>
+        /// Write a function to reverse an array
+        /// </summary>
         public static int[] Reverse(int[] array)
         {
             throw new NotImplementedException();
         }
-
+        
+        /// <summary>
+        /// Create a function that takes a string and returns the concatenated first and last character
+        /// </summary>
         public static string FirstLastCharacter(string a)
         {
             throw new NotImplementedException();
         }
-
+        
+        /// <summary>
+        /// Create a function that converts a date formatted as MM/DD/YYYY to YYYYDDMM
+        /// </summary>
         public static string FormatDateYYYYDDMM(string date)
         {
             throw new NotImplementedException();
         }
-
+        
+        /// <summary>
+        /// Create a function that returns the average of a list of numbers
+        /// </summary>
         public static double Average(int[] array)
         {
             throw new NotImplementedException();
