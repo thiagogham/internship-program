9a40a42f5004ab9a81ccc7e281aa568db98bad6b
Thiago Rodrigues Gham <thiago.gham@gmail.com>
2022-02-09 22:49:01 -0300

    Update EasyProgram.cs

--- InternshipProgram/Easy/EasyProgram.cs
+++ InternshipProgram/Easy/EasyProgram.cs
@@ -4,11 +4,20 @@ namespace InternshipProgram.Easy
 {
     public static class EasyProgram
     {
+        /// <summary>
+        /// Create a function that takes an integer and returns the factorial of that integer. That is, the integer multiplied by all positive lower integers
+        /// </summary>
         public static int Factorial(int number)
         {
             throw new NotImplementedException();
         }
-
+        
+        /// <summary>
+        /// Create a function that takes an array of numbers and returns a new array, sorted in ascending order (smallest to biggest).
+        ///     -> Sort the numbers array in ascending order.
+        ///     -> If the function's argument is null, an empty array, or undefined; return an empty array.
+        ///     -> Return a new array of sorted numbers.
+        /// </summary>
         public static int[] SortNumsAscending(int[] arrayNumbers)
         {
             throw new NotImplementedException();
